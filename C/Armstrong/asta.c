eef678f43450de3d130a07214ac0be2179fd3c5d
Mohamed Nishath <149046343+nishath1903@users.noreply.github.com>
2024-09-19 20:32:08 +0530

    Update asta.c

--- C/Armstrong/asta.c
+++ C/Armstrong/asta.c
@@ -1,1 +1,35 @@
-C
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, original, remainder, result = 0, n = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    // Count the number of digits
+    while (original != 0) {
+        original /= 10;
+        ++n;
+    }
+
+    original = num;
+
+    // Calculate the sum of the cubes of digits
+    while (original != 0) {
+        remainder = original % 10;
+        result += pow(remainder, n);
+        original /= 10;
+    }
+
+    // Check if the number is an Armstrong number
+    if (result == num) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+
+    return 0;
+}
